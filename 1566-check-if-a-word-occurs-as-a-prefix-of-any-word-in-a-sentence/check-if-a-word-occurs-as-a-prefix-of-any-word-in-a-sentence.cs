ed3e6992e477418e4d7d92433f76b9b337b30f84
muhammednoorrr <149825971+muhammednoorrr@users.noreply.github.com>
2023-11-15 08:14:39 -1000

    Time: 66 ms (12.00%) | Memory: 36.4 MB (76.00%) - LeetSync

---
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cs
@@ -0,0 +1,14 @@
+public class Solution 
+{
+    public int IsPrefixOfWord(string sentence, string searchWord) 
+    {
+        var words = sentence.Split(' ');
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].StartsWith(searchWord))
+                return i + 1;
+        }
+        return -1;
+    }
+}
