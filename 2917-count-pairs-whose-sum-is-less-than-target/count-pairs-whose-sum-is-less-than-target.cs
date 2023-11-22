b5dcfaf5ad7ff6b04e2688a76b786792ffde9ad1
muhammednoorrr <149825971+muhammednoorrr@users.noreply.github.com>
2023-11-22 12:59:58 -1000

    Time: 87 ms (67.87%) | Memory: 40.4 MB (89.37%) - LeetSync

---
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int CountPairs(IList<int> nums, int target) {
+        int count = 0;
+        for(int i=0;i<nums.Count;i++){
+            for(int j=1;j<nums.Count;j++){
+            if(i<j){
+                if(nums[i]+nums[j]<target){
+                    count++;
+                }else{
+                    continue;
+                }
+            }
+            }
+        }
+        return count;
+    }
+}
