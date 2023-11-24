f4dceeb7cc2b4d6632b26b0508bfa2489658f8aa
muhammednoorrr <149825971+muhammednoorrr@users.noreply.github.com>
2023-11-24 03:49:31 -1000

    Time: 98 ms (66.00%) | Memory: 50.2 MB (6.50%) - LeetSync

--- 2977-check-if-a-string-is-an-acronym-of-words/check-if-a-string-is-an-acronym-of-words.cs
+++ 2977-check-if-a-string-is-an-acronym-of-words/check-if-a-string-is-an-acronym-of-words.cs
@@ -1,11 +1,11 @@
 public class Solution {
     public bool IsAcronym(IList<string> words, string s) {
         int i=0;
-        char t;
+        char temp;
         string acr="";
         foreach(string str in words){
-            t=str[i];
-            acr+=t;
+            temp=str[i];
+            acr+=temp;
         }
         if(acr== s){
             return true;
