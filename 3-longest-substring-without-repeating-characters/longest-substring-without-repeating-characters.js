fb9b01211f6249f55cbe594d3aaea48dab06feec
Muhammed Ahmed <muhamme7d7@gmail.com>
2025-01-24 23:24:25 -0900

    Time: 9 ms (74.97%) | Memory: 52.7 MB (93.57%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let n = s.length;
+    let maxLength = 0;
+    let lastIndex = new Map();
+    
+    let start = 0;
+    for (let end = 0; end < n; end++) {
+        let currentChar = s[end];
+        start = Math.max(start, lastIndex.get(currentChar) || 0);
+        maxLength = Math.max(maxLength, end - start + 1);
+        lastIndex.set(currentChar, end + 1);
+    }
+    
+    return maxLength;
+};
